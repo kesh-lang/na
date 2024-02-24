04ba83f89406c981b4ab34ba47edf64b9ecfaa94
Joakim <joakim@users.noreply.github.com>
2024-02-25 00:11:49 +0100

    Add tests for compiler

---
+++ test/compiler.test.ts
@@ -0,0 +1,31 @@
+import { Glob } from 'bun'
+import { describe, test, expect } from 'vitest'
+import { compile } from '../compiler'
+import { Parser } from '../compiler/parser'
+
+const dir = `${import.meta.dir}/data`
+
+async function process(path: string, parts: string[]) {
+	const name = parts.shift()
+	if (name?.endsWith('.na')) {
+		const file = Bun.file(`${dir}/${path}`)
+		const contents = await file.text()
+		const newline = contents.indexOf('\n')
+		const expected = contents.slice(0, newline).replaceAll('\\n', '\n')
+		const actual = compile(contents.slice(newline + 1))
+		test(name.slice(0, -3), () => expect(actual).toBe(expected))
+	} else if (name) {
+		describe(name, () => process(path, parts))
+	}
+}
+
+const glob = new Glob('**/*.na')
+
+for await (const path of glob.scan('./test/data')) process(path, path.split('/'))
+
+describe('json', () => {
+	test('basic', () => {
+		const output = compile.toJSON('1 2 3')
+		expect(output).toBe('{"0": 1, "1": 2, "2": 3}')
+	})
+})
